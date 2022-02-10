93d03a43f7394ce7b16ee87a901182bf25f025b7
NikiPiggi <99394778+NikiPiggi@users.noreply.github.com>
2022-02-10 23:25:46 +0300

    Реализацию
    Реализован дэк через массив.

---
+++ GW_1/GW_1/Dec.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW_1
+{
+    public class Dec
+    {
+        int[] storge;//Массив
+        int head;//Первый элемент
+        int tail;//Последний элемент
+        int realSize;//Возможный размер
+        int size;//Размер массива
+
+        public int Head
+        {
+            get{ return head; }
+        }
+        public int Tail
+        {
+            get{ return tail; }
+        }
+        public string toString()
+        {
+            string res = "";
+            foreach (int val in storge)
+            {
+                res += Convert.ToString(val) + " ";
+            }
+            return res;
+        }
+
+        public Dec()//По умолчанию
+        {
+            this.realSize = 10;
+            this.size = 0;
+            this.storge = new int[this.size];
+            this.head = default;
+            this.tail = default;
+
+        }
+        public Dec(int realSize)//С возможным размером
+        {
+            this.realSize = realSize;
+            this.size = 0;
+            this.storge = new int[this.size];
+            this.head = default;
+            this.tail = default;
+
+        }
+        public Dec(int[] input)//С входным массивом
+        {
+            this.size = input.Count();
+            this.realSize = size * 2;
+            this.head = input[0];
+            this.tail = input[input.Count() - 1];
+            this.storge = new int[input.Count()];
+            Save(input);
+
+        }
+        void Save(int[] input)//Переносит входной массив в храмиый
+        {
+            for (int i = 0; i < input.Count(); i++)
+            {
+                this.storge[i] = input[i];
+            }
+        }
+        void CheakSize()//проверкаа возможного размера
+        {
+            if(this.size == this.realSize)
+            {
+                this.realSize = this.realSize * 2;
+            }
+        }
+        public int AddLeft(int Ell)//добавления в начало
+        {
+            CheakSize();//проверяем размер
+            this.size++;//+1 элемент
+            int[] buf = new int[this.size];//временный массив
+            buf[0] = Ell;//добовляем новый элемент
+            for(int i = 1; i < this.size; i++)//дописываем существущие
+            {
+                buf[i] = this.storge[i - 1]; 
+            }
+            this.head = Ell;//обновляем голову
+            this.storge = new int[size];//обновляем массив
+            Save(buf);//сохроняем с новым элементом
+            return Ell;
+        }
+        public int AddRight(int Ell)//по анологии
+        {
+            CheakSize();
+            this.size++;
+            int[] buf = new int[this.size];
+            buf[size - 1] = Ell;
+            for(int i = 0; i < this.size - 1; i++)
+            {
+                buf[i] = this.storge[i];
+            }
+            this.tail = Ell;
+            this.storge = new int[size];
+            Save(buf);
+            return Ell;
+        }
+        public int DeletLeft()
+        {
+            int res = this.storge[0];
+            this.size--;
+            int[] buf = new int[this.size];
+            for(int i = 0; i < size; i++)
+            {
+                buf[i] = this.storge[i + 1];
+            }
+            this.head = buf[0];
+            this.storge = new int[size];
+            Save(buf);
+            return res;
+        }
+        public int DeletRight()
+        {
+            int res = this.storge[size - 1];
+            this.size--;
+            int[] buf = new int[this.size];
+            for(int i = 0; i < this.size; i++)
+            {
+                buf[i] = this.storge[i];
+            }
+            this.tail = buf[size - 1];
+            this.storge = new int[size];
+            Save(buf);
+            return res;
+        }
+    }
+}
---
+++ GW_1/GW_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using GW_1;
+
+Dec d = new Dec(new int[] {1,2,3,4,5});
+
+Console.WriteLine(d.toString());
+Console.WriteLine(d.Head);
+Console.WriteLine(d.Tail);
+Console.WriteLine();
+
+d.AddLeft(0);
+Console.WriteLine(d.toString());
+Console.WriteLine(d.Head);
+Console.WriteLine(d.Tail);
+Console.WriteLine();
+
+d.AddRight(6);
+Console.WriteLine(d.toString());
+Console.WriteLine(d.Head);
+Console.WriteLine(d.Tail);
+Console.WriteLine();
+
+d.DeletLeft();
+Console.WriteLine(d.toString());
+Console.WriteLine(d.Head);
+Console.WriteLine(d.Tail);
+Console.WriteLine();
+
+d.DeletRight();
+Console.WriteLine(d.toString());
+Console.WriteLine(d.Head);
+Console.WriteLine(d.Tail);
+Console.WriteLine();
